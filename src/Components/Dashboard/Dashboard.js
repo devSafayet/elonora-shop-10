d3c528e147b62ab9034b0e051295d7d7689a082c
Safayet <mrishafayet003@gmail.com>
2022-04-14 22:29:06 +0600

    dashboard components done

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -24,7 +24,19 @@ const Dashboard = () => {
                 </LineChart>
 
             </div>
+            <div>
+                <div>
+                    <h3 className='text-center text-primary'>Invest Vs Revenue</h3>
+                    <PieChart width={400} height={300}>
+                        <Pie data={data} dataKey="investment" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                        <Pie data={data} dataKey="revenue" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                        <Tooltip></Tooltip>
+                        <Legend></Legend>
+                    </PieChart>
 
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import image from '../../images/images.png';
-import Review from '../Review/Review';
+
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
     return (
@@ -10,17 +11,21 @@ const Home = () => {
                 <div className="let-side  col-md-6">
                     <h2 className='text-center text-success'>Elonora</h2>
                     <p>This shop is very different. Here you can buy goods at very low prices and it is of very good quality. I hope I can do you a lot of good. You will be with us. Go with our support.</p>
-                    <button className='bg-primary  px-3 py-1 text-white rounded text-decoration-none'>See More</button>
+                    <button className='bg-primary border px-3 py-1 text-white rounded text-decoration-none'>See More</button>
                 </div>
                 <div className="right-side col-md-6 ps-5">
                     <img src={image} alt="" />
                 </div>
             </section>
-            <div className="mt-5">
+
+            {/* <div className="mt-5">
                 <Review></Review>
-                <Link className='bg-primary  px-5 py-2 text-white rounded text-decoration-none  justify-content-center' to="/review">See Review All</Link>
             </div>
+            <div className=' see-btn'>
+                <Link className='bg-primary  px-5 py-2 text-white rounded text-decoration-none ' to="/review">See Review All</Link>
+            </div> */}
         </div>
+
 
 
     );
